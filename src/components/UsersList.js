b999ccf3d504049584ab31240529cb145e3b3d35
Ashiqur Rahman <s.m.rahman6666@gmail.com>
2023-05-20 02:46:08 +0600

    Async Method added

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,49 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { fetchUsers, addUser } from "../store";
+import Skeleton from "./Skeleton";
+import Button from "./Button";
+import { useThunk } from "../hooks/use-thunk";
+import UsersListItem from "./UsersListItem";
+
 function UsersList() {
-  return <>UsersList</>;
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+  const { data } = useSelector((state) => {
+    return state.users;
+  });
+
+  useEffect(() => {
+    doFetchUsers();
+  }, [doFetchUsers]);
+
+  const handleAddUser = () => {
+    doCreateUser();
+  };
+
+  let content;
+  if (isLoadingUsers) {
+    content = <Skeleton times={6} className="h-20 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error...</div>;
+  } else {
+    content = data.map((user) => {
+      return <UsersListItem key={user.id} user={user} />;
+    });
+  }
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between items-center m-3">
+        <h3 className="m-2 text-xl">List Users</h3>
+        <Button loading={isCreatingUser} onClick={handleAddUser}>
+          + Add User
+        </Button>
+        {creatingUserError && "Creating User Error ..."}
+      </div>
+      {content}
+    </div>
+  );
 }
 export default UsersList;
